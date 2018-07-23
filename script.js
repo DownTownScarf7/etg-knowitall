56bb408cbccf88ed2a00a9ca40afa38fa56fe51c
DownTownScarf7 <vbence741@gmail.com>
2018-07-24 00:07:30 +0200

    added basic script file

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.onload = () => {
+  document.querySelector('#gungeoneers').appendChild(document.createElement('h3')).innerText = 'GUNGONEERS';
+  document.querySelector('#guns').appendChild(document.createElement('h3')).innerText = 'GUNS';
+  document.querySelector('#items').appendChild(document.createElement('h3')).innerText = 'ITEMS';
+  document.querySelector('#npcs').appendChild(document.createElement('h3')).innerText = 'NPCS';
+  document.querySelector('#bosses').appendChild(document.createElement('h3')).innerText = 'BOSSES';
+  document.querySelector('#cult').appendChild(document.createElement('h3')).innerText = 'CULT OF THE GUNDEAD';
+  document.querySelector('#objects').appendChild(document.createElement('h3')).innerText = 'OBJECTS';
+  document.querySelector('#pickups').appendChild(document.createElement('h3')).innerText = 'PICKUPS';
+}
