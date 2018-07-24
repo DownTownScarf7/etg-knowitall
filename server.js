a0bf47235971a7ae39587cd18d0ecb4b6c93c8fb
DownTownScarf7 <vbence741@gmail.com>
2018-07-24 13:24:59 +0200

    implemented first test

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
+const cheerio = require('cheerio');
+const request = require('request');
 const app = express();
 const PORT = 8080;
 
@@ -17,8 +19,31 @@ app.use(express.json());
 //   database: process.env.DB_DATABASE,
 // });
 
+const urlGungeoneers = 'https://enterthegungeon.gamepedia.com/Gungeoneers';
+const iconGungeoneers = [];
+
+request(urlGungeoneers, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const $ = cheerio.load(body);
+  $('#mw-content-text table tr').each(function () {
+    if ($(this).children('td').length > 0) {
+      iconGungeoneers.push($(this).children('td').eq(0).find('img').attr('src'));
+    }
+  });
+  console.log(iconGungeoneers);
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    iconGungeoneers,
+  });
 });
 
 app.listen(PORT, () => {
