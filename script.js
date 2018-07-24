002e09179c3daee976c10e51780d3b9980fc27f4
DownTownScarf7 <vbence741@gmail.com>
2018-07-24 13:25:33 +0200

    added gungeoneer icons

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 'use strict';
 
+const api = 'http://localhost:8080/api';
+const requestGungeoneers = new XMLHttpRequest();
+
+
 window.onload = () => {
-  document.querySelector('#gungeoneers').appendChild(document.createElement('h3')).innerText = 'GUNGONEERS';
+  requestGungeoneers.open('GET', api, true);
+  requestGungeoneers.setRequestHeader("Content-Type", "application/json");
+  requestGungeoneers.onload = () => {
+    const res = JSON.parse(requestGungeoneers.responseText);
+    console.log(res);
+    document.querySelector('#gungeoneers').appendChild(document.createElement('h3')).innerText = 'GUNGONEERS';
+    res.iconGungeoneers.forEach(icon => {
+      document.querySelector('#gungeoneers').appendChild(document.createElement('img')).src = icon;
+    });
+  };
+  requestGungeoneers.send();
+
+  document.querySelector('#search').addEventListener('keydown', event => {
+    console.log(event.key);
+  });
+
   document.querySelector('#guns').appendChild(document.createElement('h3')).innerText = 'GUNS';
   document.querySelector('#items').appendChild(document.createElement('h3')).innerText = 'ITEMS';
   document.querySelector('#npcs').appendChild(document.createElement('h3')).innerText = 'NPCS';
