11e1e22d3fe92bf9a673e711830da85171d54031
DownTownScarf7 <vbence741@gmail.com>
2018-07-24 15:14:14 +0200

    modified DOM according to server

--- script.js
+++ script.js
@@ -10,15 +10,31 @@ window.onload = () => {
   requestGungeoneers.onload = () => {
     const res = JSON.parse(requestGungeoneers.responseText);
     console.log(res);
-    document.querySelector('#gungeoneers').appendChild(document.createElement('h3')).innerText = 'GUNGONEERS';
-    res.iconGungeoneers.forEach(icon => {
-      document.querySelector('#gungeoneers').appendChild(document.createElement('img')).src = icon;
-    });
+    const mainDiv = document.querySelector('#gungeoneers');
+    const subDiv = document.createElement('div');
+    mainDiv.appendChild(document.createElement('h3')).innerText = 'GUNGONEERS';
+    subDiv.classList.add('icon-container');
+    mainDiv.appendChild(subDiv);
+    for (let i in res.dataGungeoneers) {
+      const gungeoneer = res.dataGungeoneers[i];
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+      img.classList.add('icon', 'gungeoneer');
+      img.src = gungeoneer.icon;
+      subDiv.appendChild(div);
+      div.appendChild(img);
+    }
   };
   requestGungeoneers.send();
 
   document.querySelector('#search').addEventListener('keydown', event => {
     console.log(event.key);
+  });
+
+  document.querySelector('#response-container').addEventListener('mouseover', event => {
+    if (event.target.classList.contains('icon')) {
+      console.log(event.target);
+    }
   });
 
   document.querySelector('#guns').appendChild(document.createElement('h3')).innerText = 'GUNS';
